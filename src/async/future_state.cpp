2aa1346445ffeba675ba60e4a57f48e4c366f249
Hugh Shin <hughbee135@gmail.com>
2026-04-04 22:41:32 -0700

    Implement Future

--- src/async/future_state.cpp
+++ src/async/future_state.cpp
@@ -1,15 +1,87 @@
 #include "future_state.h"
 
+#include "thread_local_state.h"
+
+#include "telemetry/living_span.h"
+
+#include <condition_variable>
+#include <functional>
+#include <memory>
+#include <mutex>
+#include <utility>
+#include <vector>
+
 // -----------------------------------------------------------------------------
 
 void server::impl::FutureState<void>::Fulfill()
 {
-  // TODO
+  std::vector<std::function<void()>> onFulfillCallbacks;
+  {
+    std::lock_guard lock{ m_mutex };
+    m_fulfilled = true;
+    onFulfillCallbacks.swap(m_onFulfillCallbacks);
+  }
+
+  for (std::function<void()>& onFulfillCallback : onFulfillCallbacks)
+  {
+    onFulfillCallback();
+  }
 }
 
 // -----------------------------------------------------------------------------
 
 void server::impl::FutureState<void>::Await()
 {
-  // TODO
+  ThreadLocalCoroutineContext* context = GetThreadLocalCoroutineContext();
+
+  std::unique_lock futureLock{ m_mutex };
+
+  if (m_fulfilled)
+  {
+    return;
+  }
+
+  if (!context)
+  {
+    bool threadReady = false;
+    std::mutex threadReadyMutex;
+    std::condition_variable threadReadyCv;
+
+    m_onFulfillCallbacks.push_back([
+      &threadReady,
+      &threadReadyMutex,
+      &threadReadyCv]()
+      {
+        {
+          std::lock_guard threadReadyLock{ threadReadyMutex };
+          threadReady = true;
+        }
+        threadReadyCv.notify_one();
+      });
+
+    futureLock.unlock();
+
+    std::unique_lock threadReadyLock{ threadReadyMutex };
+    while (!threadReady)
+    {
+      threadReadyCv.wait(threadReadyLock);
+    }
+  }
+
+  else
+  {
+    m_onFulfillCallbacks.push_back(std::move(context->m_requeueCallback));
+
+    futureLock.unlock();
+
+    std::unique_ptr<LivingSpan> span{ std::move(context->m_span) };
+    context->m_span.reset();
+    std::function<void()> yieldCallback = std::move(context->m_yieldCallback);
+    context->m_yieldCallback = {};
+
+    yieldCallback();
+
+    context->m_yieldCallback = std::move(yieldCallback);
+    context->m_span = std::move(span);
+  }
 }
--- src/async/future_state.h
+++ src/async/future_state.h
@@ -47,14 +47,31 @@ private:
 
 // -----------------------------------------------------------------------------
 
-// TODO: #includes
+#include "thread_local_state.h"
+
+#include "telemetry/living_span.h"
+
+#include <condition_variable>
+#include <memory>
+#include <utility>
 
 // -----------------------------------------------------------------------------
 
 template <typename T>
 void server::impl::FutureState<T>::Fulfill(T&& result)
 {
-  // TODO
+  std::vector<std::function<void()>> onFulfillCallbacks;
+  {
+    std::lock_guard lock{ m_mutex };
+    m_result = std::move(result);
+    m_fulfilled = true;
+    onFulfillCallbacks.swap(m_onFulfillCallbacks);
+  }
+
+  for (std::function<void()>& onFulfillCallback : onFulfillCallbacks)
+  {
+    onFulfillCallback();
+  }
 }
 
 // -----------------------------------------------------------------------------
@@ -62,6 +79,58 @@ void server::impl::FutureState<T>::Fulfill(T&& result)
 template <typename T>
 const T& server::impl::FutureState<T>::Await()
 {
-  // TODO
+  ThreadLocalCoroutineContext* context = GetThreadLocalCoroutineContext();
+
+  std::unique_lock futureLock{ m_mutex };
+
+  if (m_fulfilled)
+  {
+    return *m_result;
+  }
+
+  if (!context)
+  {
+    bool threadReady = false;
+    std::mutex threadReadyMutex;
+    std::condition_variable threadReadyCv;
+
+    m_onFulfillCallbacks.push_back([
+      &threadReady,
+      &threadReadyMutex,
+      &threadReadyCv]()
+      {
+        {
+          std::lock_guard threadReadyLock{ threadReadyMutex };
+          threadReady = true;
+        }
+        threadReadyCv.notify_one();
+      });
+
+    futureLock.unlock();
+
+    std::unique_lock threadReadyLock{ threadReadyMutex };
+    while (!threadReady)
+    {
+      threadReadyCv.wait(threadReadyLock);
+    }
+  }
+
+  else
+  {
+    m_onFulfillCallbacks.push_back(std::move(context->m_requeueCallback));
+
+    futureLock.unlock();
+
+    std::unique_ptr<LivingSpan> span{ std::move(context->m_span) };
+    context->m_span.reset();
+    std::function<void()> yieldCallback = std::move(context->m_yieldCallback);
+    context->m_yieldCallback = {};
+
+    yieldCallback();
+
+    context->m_yieldCallback = std::move(yieldCallback);
+    context->m_span = std::move(span);
+  }
+
   return *m_result;
 }
