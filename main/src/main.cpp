11dd1074f4dc6b3377cf5235aef2d7f4ef7fda80
Hugh Shin <hughbee135@gmail.com>
2026-05-16 19:48:11 -0700

    Integrate runtime into program entry point

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,4 +1,25 @@
-int main()
+#include "async/async.h"
+
+#include <functional>
+
+// -----------------------------------------------------------------------------
+
+static int Application(
+    int argc, char *argv[]);
+
+// -----------------------------------------------------------------------------
+
+int main(
+    int argc, char *argv[])
 {
+  return async::RunApplication(Application, argc, argv);
+}
+
+// -----------------------------------------------------------------------------
+
+static int Application(
+    int /*argc*/, char * /*argv*/[])
+{
+  // Entry point for application logic
   return 0;
 }
--- test/src/test.cpp
+++ test/src/test.cpp
@@ -1,4 +1,24 @@
-int main()
+#include "async/async.h"
+
+#include <functional>
+
+// -----------------------------------------------------------------------------
+
+static int Application(
+    int argc, char *argv[]);
+
+// -----------------------------------------------------------------------------
+
+int main(
+    int argc, char *argv[])
+{
+  return async::RunApplication(Application, argc, argv);
+}
+
+// -----------------------------------------------------------------------------
+
+static int Application(
+    int /*argc*/, char * /*argv*/[])
 {
   return 0;
 }
